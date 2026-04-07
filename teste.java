587ea9117aab9e2b58b9966da6d5c413d8a721a3
agathaWB <agathaisabelliw@gmail.com>
2026-04-07 09:21:08 -0300

    feito

--- teste.java
+++ teste.java
@@ -1,5 +1,7 @@
 public class teste {
     public static void main(String[] args) {
-        System.out.println("teste de comite");
+        System.out.println("teste de alguma coisa");
+         System.out.println("teste de alguma coisa");
+    
     }
 }
